546f85831ace056370f928df9d29f25b7d276e1a
nedoprogramist89 <145318384+nedoprogramist89@users.noreply.github.com>
2024-06-01 00:05:09 +0300

    Add files via upload

---
+++ DLLD/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+typedef bool(*FindCharsFunc)(const char*, const char*);
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    std::cout << "Введите строку: ";
+    std::string str;
+    std::getline(std::cin, str);
+    std::cout << "Введите символы для поиска: ";
+    std::string chars;
+    std::getline(std::cin, chars);
+
+    HMODULE lib = LoadLibrary(L"DLLD.dll");
+    if (!lib) {
+        std::cerr << "Ошибка загрузки библиотеки" << std::endl;
+        return 1;
+    }
+
+    FindCharsFunc proc = reinterpret_cast<FindCharsFunc>(GetProcAddress(lib, "FindChars"));
+    if (!proc) {
+        std::cerr << "Ошибка" << std::endl;
+        FreeLibrary(lib);
+        return 1;
+    }
+    bool result = proc(str.c_str(), chars.c_str());
+    std::cout << "Символы найдены: " << (result ? "true" : "false") << std::endl;
+
+    return 0;
+}
---
+++ DLLD/DLLD/dllmain.cpp
@@ -0,0 +1,10 @@
+﻿#include <cstring>
+
+extern "C" __declspec(dllexport) bool FindChars(const char* str, const char* chars) {
+    for (const char* p = chars; *p; ++p) {
+        if (!strchr(str, *p)) {
+            return false;
+        }
+    }
+    return true;
+}
